3b7d8751a4284bf96ffe7d1f3d0d09731e7251cf
Boyi Guo <boyiguo1@uab.edu>
2020-01-10 08:54:15 -0600

    Refactor Susan's method

--- Code/susan_method.R
+++ Code/susan_method.R
@@ -1,15 +1,15 @@
 library(glmnet)
 
 # Constructing data with interaction term
-dat <- data.frame( x = X.train.base, Treat = Treat.train)
+dat <- data.frame( x = sim.dat$train$x.b, Treat = sim.dat$train$treat)
 f <- as.formula(~(.-Treat)*Treat)
 x <- model.matrix(f, dat)
 
-cv.res <- cv.glmnet(x,Y.train.end,family="mgaussian", standardize=T, intercept=T)
-glm.res <- glmnet(x,Y.train.end,family="mgaussian", lambda = cv.res$lambda.min, intercept=T)
+cv.res <- cv.glmnet(x,sim.dat$train$y.e,family="mgaussian", standardize=T, intercept=T)
+glm.res <- glmnet(x,sim.dat$train$y.e,family="mgaussian", lambda = cv.res$lambda.min, intercept=T)
 
-test.treat <- data.frame(x=X.test.base, Treat=rep(1,n.test))
-test.untreat <- data.frame(x=X.test.base, Treat=rep(0,n.test))
+test.treat <- data.frame(x=sim.dat$test$x.b, Treat=rep(1,n.test))
+test.untreat <- data.frame(x=sim.dat$test$x.b, Treat=rep(0,n.test))
 
 x.test.treat <- model.matrix(f, test.treat)
 x.test.untreat <- model.matrix(f, test.untreat)
